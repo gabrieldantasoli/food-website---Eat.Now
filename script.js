60cd5a6524c019b5a61fce675c7eb2de17070164
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-01-16 18:13:24 -0300

    first files website page

---
+++ script.js
@@ -0,0 +1,35 @@
+let bar = document.querySelector('.fa-bars');
+let navbar = document.querySelector('.navbar');
+let changecolor = document.querySelector('#changecolor');
+
+bar.addEventListener('click',function(e) {
+    if (e.target.classList == 'fas fa-bars') {
+        navbar.classList.add('active');
+        bar.classList.replace('fa-bars','fa-times')
+    }else if (e.target.classList == 'fas fa-times'){
+        navbar.classList.remove('active');
+        bar.classList.replace('fa-times','fa-bars')
+    }
+})
+
+changecolor.addEventListener('click',function(e) {
+    if (e.target.classList == 'fas fa-moon'){
+        document.body.classList.add('active');
+        changecolor.classList.replace('fa-moon','fa-sun')
+    }else if (e.target.classList == 'fas fa-sun'){
+        document.body.classList.remove('active');
+        changecolor.classList.replace('fa-sun','fa-moon')
+    }
+})
+
+window.onscroll = (e) => {
+    navbar.classList.remove('active');
+    bar.classList.replace('fa-times','fa-bars')
+}
+
+window.onresize = () => {
+    if (window.innerWidth >= 750) {
+        navbar.classList.remove('active');
+        bar.classList.replace('fa-times','fa-bars')
+    }
+}
